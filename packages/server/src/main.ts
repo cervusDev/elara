b7c6f0c50eae89d64bc755a22c7d80dc896ac6eb
Gustavo Henrique <gustavo.cervus@gmail.com>
2022-04-13 14:42:54 -0300

    feat(configs): :sparkles: validator configs in main.ts
    initial configs using class-validator library on main.ts

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,8 +1,20 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { useContainer } from 'class-validator';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      errorHttpStatusCode: 422,
+      transform: true,
+    }),
+  );
   await app.listen(8080);
 }
 
