eb15aa43f5a36365ce7b81039eb9c57710cdfdb4
Gustavo Henrique <gustavo.cervus@gmail.com>
2022-04-14 10:52:58 -0300

    ci(configs): :sparkles: initial configs on app.module

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Global, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 
+@Global()
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    HttpModule,
+  ],
+  exports: [HttpModule],
 })
 export class AppModule {}
