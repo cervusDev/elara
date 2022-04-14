8ceaf853e32198e78a9f784d92607c14768ae576
Gustavo Henrique <gustavo.cervus@gmail.com>
2022-04-14 11:52:58 -0300

    ✨ implements share layer on serve

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -3,15 +3,17 @@ import { ConfigModule } from '@nestjs/config';
 import { HttpModule } from '@nestjs/axios';
 import { PrismaModule } from 'infra/database/prisma/prisma.module';
 import { PresenterModule } from 'presenter/presenter.module';
+import { IocModule } from 'share/ioc/ioc.module';
 
 @Global()
 @Module({
   imports: [
+    IocModule,
     HttpModule,
     PrismaModule,
     PresenterModule,
     ConfigModule.forRoot({ isGlobal: true }),
   ],
-  exports: [HttpModule],
+  exports: [HttpModule, IocModule],
 })
 export class AppModule {}
