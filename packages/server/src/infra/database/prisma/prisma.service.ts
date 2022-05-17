67ec9cd0d0beb11cab7b82d5a9598a00f935c76a
Gustavo Henrique <gustavo.cervus@gmail.com>
2022-05-17 08:08:15 -0300

    refactor(refactor on prisma service): :sparkles:

--- packages/server/src/infra/database/prisma/prisma.service.ts
+++ packages/server/src/infra/database/prisma/prisma.service.ts
@@ -1,70 +1,15 @@
-import {
-  Injectable,
-  OnModuleInit,
-  Logger,
-  INestApplication,
-} from '@nestjs/common';
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  logger = new Logger(PrismaService.name);
-
-  constructor() {
-    super({
-      log: [
-        {
-          emit: 'event',
-          level: 'query',
-        },
-      ],
-    });
-  }
-
   async onModuleInit() {
     await this.$connect();
-
-    this.$on('query' as any, async (event: any) => {
-      this.logger.debug(`${event.duration}ms) ${event.query}`);
-    });
   }
 
   async enableShutdownHooks(app: INestApplication) {
     this.$on('beforeExit', async () => {
       await app.close();
-    });
-  }
-
-  async truncate() {
-    const records = await this.$queryRawUnsafe<Array<any>>(`SELECT tablename
-                                                          FROM pg_tables
-                                                          WHERE schemaname = 'public'`);
-    records.forEach((record) => this.truncateTable(record.tablename));
-  }
-
-  async truncateTable(tablename) {
-    if (tablename === undefined || tablename === '_prisma_migrations') {
-      return;
-    }
-    try {
-      await this.$executeRawUnsafe(
-        `TRUNCATE TABLE "public"."${tablename}" CASCADE;`,
-      );
-    } catch (error) {
-      // console.log({ error });
-    }
-  }
-
-  async resetSequences() {
-    const results = await this.$queryRawUnsafe<Array<any>>(
-      `SELECT c.relname
-       FROM pg_class AS c
-                JOIN pg_namespace AS n ON c.relnamespace = n.oid
-       WHERE c.relkind = 'S'
-         AND n.nspname = 'public'`,
-    );
-    results.forEach(({ record }) => {
-      this.$executeRawUnsafe(`ALTER SEQUENCE "${record.relname}" RESTART WITH 1;`);
     });
   }
 }
